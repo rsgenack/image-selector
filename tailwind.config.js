09764cabb5ca6c2c210f8cb3ab080a1d4c12a5df
rsgenack <rsgenack@gmail.com>
2025-10-30 16:08:37 -0400

    Switch PostCSS/Tailwind configs to ESM and install tailwindcss-animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     "./app/**/*.{js,jsx}",
@@ -146,5 +148,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
